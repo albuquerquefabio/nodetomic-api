3676cf2cfdd52dc900423faeeb781566e312dcb9
Fábio Albuquerque <albuquerquefabio@icloud.com>
2020-04-27 20:08:41 -0300

    Original code to socket

--- src/api/sockets/example.socket.js
+++ src/api/sockets/example.socket.js
@@ -1,7 +1,3 @@
-import ExampleEvents from "../events/example.events";
-
-const events = ["add", "delete"];
-
 export let socket = null;
 export let io = null;
 
@@ -14,20 +10,10 @@ export default (_socket, _io) => {
 
 // Here should be all events 'on'
 export function on() {
-  for (const key in events) {
-    const event = events[key];
-    const listener = createListener(`example:${event}`, socket);
-    ExampleEvents.on(event, listener);
-    socket.on(`example:${event}`, (data) => emit(`example:${event}`, data));
-    socket.on("disconnect", removeListener(event, listener));
-  }
-  // socket.on('example:delete', data => emit('delete', data));
+  socket.on("example:add", (data) => emit("add", data));
+  socket.on("example:delete", (data) => emit("delete", data));
 }
 
-// eslint-disable-next-line no-shadow
-const createListener = (event, socket) => (data) => io.emit(event, data);
-const removeListener = (event, listener) => () =>
-  ExampleEvents.removeListener(event, listener);
 // Emit events
 export function emit(event, data) {
   io.emit(event, data);
