e67714cfd122dddb5cd666d0ffdce71fa080b5db
Fábio Albuquerque <albuquerquefabio@icloud.com>
2020-04-27 19:06:02 -0300

    Remove model events

--- src/api/models/example.model.js
+++ src/api/models/example.model.js
@@ -1,19 +1,19 @@
 import mongoose from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate';
-import { registerEvents } from '../events/example.events';
+
 const Schema = mongoose.Schema;
 
 const ExampleSchema = new Schema({
   greet: {
     type: String,
-    required: [true, 'Greet is required.']
+    required: [true, 'Greet is required.'],
   },
   language: {
     type: String,
-    required: [true, 'Language is required.']
-  }
+    required: [true, 'Language is required.'],
+  },
 });
-registerEvents(ExampleSchema);
+
 ExampleSchema.plugin(mongoosePaginate);
 
 export default mongoose.model('Example', ExampleSchema);
